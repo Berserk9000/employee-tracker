c8514c4881faf8ae4247856fc90c13773e90908a
Berserk9000 <cruz.d.kevin@gmail.com>
2022-08-31 20:59:11 -0400

    setting up inquirer for user selection

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//Dependencies found here
+//Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
 const db = require(".");
 
-  //Connection
+//Connection
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -13,7 +13,60 @@ const connection = mysql.createConnection({
   database: "employee_info_db"
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+  startScreen();
+});
 
 
+  //Selection
+  function startScreen() {
+    inquirer
+      .prompt({
+        type: "list",
+        choices: [
+        "View departments", 
+        "View roles",
+        "View employees",
+        "Add department",
+        "Add role",
+        "Add employee",
+        "Update employee role",
+        "Quit"
+        ],
+        message: "What would you like to do?",
+        name: "option"
+      })
+      .then(function(result) {
+        console.log("You entered: " + result.option);
+  
+        switch (result.option) {
+          case "Add department":
+            addDepartment();
+            break;
+          case "Add role":
+            addRole();
+            break;
+          case "Add employee":
+            addEmployee();
+            break;
+          case "View departments":
+            viewDepartment();
+            break;
+          case "View roles":
+            viewRoles();
+            break;
+          case "View employees":
+            viewEmployees();
+            break;
+          case "Update employee role":
+            updateEmployee();
+            break;
+          default:
+            quit();
+        }
+      });
+  }
 
 
+  
